4e37a64fd8e46219c709115da7206d72ddaa4779
sonalpant5 <sonalpant5@gmail.com>
2025-07-25 18:05:35 -0500

    added print statement in Test1

--- git_sampleproject/src/test/java/sampletestcases/Test1.java
+++ git_sampleproject/src/test/java/sampletestcases/Test1.java
@@ -3,6 +3,7 @@ package sampletestcases;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		System.out.println("This is a sample test case.");
 	}
 
 }
