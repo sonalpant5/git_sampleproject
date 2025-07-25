086876f2ab0c229ee6183a44b3cc7961f5a6341b
sonalpant5 <sonalpant5@gmail.com>
2025-07-25 18:20:28 -0500

    Update Test2.java
    deleted some prinln lines inTest2

--- git_sampleproject/src/test/java/sampletestcases/Test2.java
+++ git_sampleproject/src/test/java/sampletestcases/Test2.java
@@ -3,12 +3,6 @@ package sampletestcases;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println("This is a sample test case.");
-		System.out.println("checking changes in Test2.");
-		System.out.println("Adding another line for testing.");
-		System.out.println("Finalizing the test case.");
-		System.out.println("End of Test2.");
 		System.out.println("This is the last line in Test2.");
 		System.out.println("Goodbye from Test2!");
 		System.out.println("Test2 completed successfully.");
