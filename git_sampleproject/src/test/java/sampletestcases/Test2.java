c04b2a0f1ebe5a2fb9577f523d8be6652b88a685
sonalpant5 <sonalpant5@gmail.com>
2025-07-28 13:05:17 -0500

    Update Test2.java

--- git_sampleproject/src/test/java/sampletestcases/Test2.java
+++ git_sampleproject/src/test/java/sampletestcases/Test2.java
@@ -3,11 +3,8 @@ package sampletestcases;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is the last line in Test2.");
-		System.out.println("Goodbye from Test2!");
-		System.out.println("Test2 completed successfully.");
-		System.out.println("No more changes needed.");
-		System.out.println("Exiting Test2 now.");
+		System.out.println("This is Test2.");
+		
 
 	}
 
